00d9af5bb081def6182ee4a41ac5e353a2ccda02
tipurk <yura.abushkevich@mail.ru>
2024-11-25 23:51:15 +0300

    huh?

--- views/navbotbar/script.js
+++
@@ -1,9 +0,0 @@
-// Функция для обработки кликов и установки активного элемента
-function selectNavItem(item) {
-  // Убираем класс active со всех элементов
-  const navItems = document.querySelectorAll(".bottom-nav .nav-item");
-  navItems.forEach((el) => el.classList.remove("active"));
-
-  // Добавляем класс active к выбранному элементу
-  item.classList.add("active");
-}
